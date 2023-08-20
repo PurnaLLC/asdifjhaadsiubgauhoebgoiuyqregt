a3c04c49b48adf5c07e43aef0ddd90267fc04173
ThunderSmoke <116113740+ThunderSmoke@users.noreply.github.com>
2023-08-20 15:40:02 -0500

    Initial Commit

---
+++ Self Help/Self_HelpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Self_HelpApp.swift
+//  Self Help
+//
+//  Created by Maxwell Meyer on 8/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Self_HelpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
