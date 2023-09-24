af127854839ecef1e53aa02fd52d83d43032f089
ThunderSmoke <116113740+ThunderSmoke@users.noreply.github.com>
2023-09-23 19:19:40 -0500

    dd

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -21,6 +21,9 @@ class AppDelegate: NSObject, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
+    
+    
+    //sus
+    
 }
 
